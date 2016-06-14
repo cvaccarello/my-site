0657ea670ed5be658cff9e85e40e590facc027a7
chris vaccarello <chris vaccarello>
2016-06-14 16:35:50 -0400

    tweaked the timeline to work better with the new graphics.  Added rotating moon and gas giant planet.

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -1,12 +1,43 @@
 import Ember from 'ember';
 
+function getRotationDegrees(obj) {
+    var matrix = obj.css("-webkit-transform") ||
+    obj.css("-moz-transform")    ||
+    obj.css("-ms-transform")     ||
+    obj.css("-o-transform")      ||
+    obj.css("transform");
+	var angle;
+    if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { angle = 0; }
+    //return (angle < 0) ? angle + 360 : angle;
+     return angle;
+}
+
+
 // initialize skrollr plugin
 var s = skrollr.init({
 	forceHeight: false,
 	constants: {
-		vh: function() {
-			return $('.history-bar-line').outerHeight();
+		shb_line: function() {
+			return $('#skrollr-history-bar-line').outerHeight();
 		}
+	},
+	render(options) {
+		// get all pivot points so that their children, if they exist, can be counter-rotated
+		var $moon = $('.hp-moon');
+		$moon.each(function() {
+			let $this = $(this);
+			let $parent = $this.parent();
+			let rotation = 0 - getRotationDegrees($parent);
+
+			$this.css({
+				'transform': 'rotate(' + rotation + 'deg)'
+			});
+		});
 	}
 });
 
